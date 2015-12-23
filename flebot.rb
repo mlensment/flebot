ad4a70f0781155e6ea119cd22b10b59f99f17c0c
Martin Lensment <mlensment@gmail.com>
2015-12-23 20:58:26 +0200

    Refactor

--- flebot.rb
+++ flebot.rb
@@ -1,11 +1,34 @@
 #!/usr/bin/env ruby
 require 'pry'
 Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require file }
-#
+
 class Flebot
   class << self
     def help
       'show flebot help here'
+    end
+
+    def listen
+      puts 'INFO: Starting Flebot'
+      api = Api.new
+      api.poll_messages do |raw_msg|
+        conv_id = raw_msg['conversation_id']
+        msg_body = Nokogiri::HTML(raw_msg['message']).text
+        next unless msg_body.start_with?('flebot')
+
+        app_class = find_app(msg_body)
+        unless app_class
+          api.send_message(conv_id, help)
+          next
+        end
+
+        sender = api.get_contact_email(raw_msg['account_id'])
+        members = api.get_conversation_members(conv_id)
+
+        app = app_class.new(msg_body, sender, members)
+        response = app.execute
+        api.send_message(conv_id, response)
+      end
     end
 
     def find_app(msg_body)
@@ -20,22 +43,4 @@ class Flebot
   end
 end
 
-api = Api.new
-api.poll_messages do |raw_msg|
-  conv_id = raw_msg['conversation_id']
-  msg_body = Nokogiri::HTML(raw_msg['message']).text
-  next unless msg_body.start_with?('flebot')
-
-  app_class = Flebot.find_app(msg_body)
-  unless app_class
-    api.send_message(conv_id, Flebot.help)
-    next
-  end
-
-  sender = api.get_contact_email(raw_msg['account_id'])
-  members = api.get_conversation_members(conv_id)
-
-  app = app_class.new(msg_body, sender, members)
-  response = app.execute
-  api.send_message(conv_id, response)
-end
+Flebot.listen
